2aa370fe605753d926004541d78738878bb5ba2b
Ash121205 <aasheshkaran20@gmail.com>
2022-12-05 17:29:33 +0530

    added notes and codes

---
+++ Linked List/Linked_List_Intro.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+  
+    // Default constructor
+    Node()
+    {
+        data = 0;
+        next = NULL;
+    }
+  
+    // Parameterised Constructor
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+class Solution {
+  public:
+    Node* constructLL(vector<int>& arr) {
+        // code here
+        int n = arr.size();
+        Node *head;
+        Node *temp = new Node(arr[0]);
+        head = temp;
+        for(int i=1;i<n;i++)
+        {
+            Node* t = new Node(arr[i]);
+            temp->next = t;
+            temp = temp->next;
+        }
+        
+        return head;
+    }
+};
+
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++) cin >> arr[i];
+        Solution obj;
+        Node* ans = obj.constructLL(arr);
+        while (ans) {
+            cout << ans->data << " ";
+            ans = ans->next;
+        }
+        cout << "\n";
+    }
+    return 0;
+}
